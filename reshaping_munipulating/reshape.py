6c4a827c8e6dcb8bb57c3ac755dbef9f31dcd82a
Farhanbuilds88 <farhanali510594@gmail.com>
2026-03-27 00:21:19 +0500

    Added reshaping and manipulating NumPy practice

---
+++ reshaping_munipulating/reshape.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr=np.array([10,20,34,56,78,74])
+reshaped_arr=arr.reshape(2,3)
+print(reshaped_arr)
+
+
+first_arr=np.array([23,45,6,7,8,9,0,3])
+reshaped_first=first_arr.reshape(4,2)
+print(reshaped_first)
