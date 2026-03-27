331776797f6a63428be28ded34301627f2fa6bda
Farhanbuilds88 <farhanali510594@gmail.com>
2026-03-28 02:10:40 +0500

    Added single.py in Broadcasting folder

---
+++ Broadcasting/single.py
@@ -0,0 +1,14 @@
+import numpy as np
+arr=np.array([100,200,300])
+result=arr*2
+print(result)
+
+arr=np.array([1,2,3])
+print(arr+5)
+
+arr=np.array([10,20,30])
+print(arr-3)
+
+prices=np.array([100,200,300])
+discounted=prices-20
+print(discounted)
