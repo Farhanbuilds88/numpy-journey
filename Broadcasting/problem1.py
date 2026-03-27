902ca23765dcf7320001aef3c3adb2bfb61ca0a3
Farhanbuilds88 <farhanali510594@gmail.com>
2026-03-28 01:58:03 +0500

    Added Broadcasting folder

---
+++ Broadcasting/problem1.py
@@ -0,0 +1,8 @@
+prices = [100, 200, 300]
+discount = 10
+final_prices = []
+
+for price in prices:
+    discounted_price = price - (price * discount / 100)
+    final_prices.append(discounted_price)
+print(final_prices)
