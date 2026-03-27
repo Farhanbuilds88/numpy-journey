51e8df8aaa4d026de3b62d8e0c37750194c8c2aa
Farhanbuilds88 <farhanali510594@gmail.com>
2026-03-28 02:45:53 +0500

    Added error.py in Broadcasting folder

---
+++ Broadcasting/error.py
@@ -0,0 +1,16 @@
+import numpy as np
+arr1=np.array([[1,2,4,6],[4,5,6]])
+arr2=np.array([1,3])
+result=arr1+arr2
+print(result)
+
+arr1 = np.array([[1,2,4,6],[4,5,6]])
+arr2 = np.array([1,3])
+result = arr1 + arr2
+print(result)
+
+arr1 = np.array([[1, 2, 4, 6],
+                 [4, 5, 6, 8]])
+arr2 = np.array([1, 3])
+result = arr1 + arr2
+print(result)
