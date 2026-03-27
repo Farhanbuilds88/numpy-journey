50bce120c9d769585548c8fefb32626392aef0c8
Farhanbuilds88 <farhanali510594@gmail.com>
2026-03-28 02:44:01 +0500

    Added 1d_to_2d.py in Broadcasting folder

---
+++ Broadcasting/1d_to_2d.py
@@ -0,0 +1,26 @@
+import numpy as np
+matrix=np.array([[1,3,4],[3,5,8]])
+vector=np.array([10,20,30])
+result=matrix+vector
+print(result)
+
+matrix = np.array([[2, 4, 6],
+                   [1, 3, 5]])
+
+vector = np.array([100, 200, 300])
+result = matrix + vector
+print(result)
+
+matrix = np.array([[5, 10, 15],
+                   [20, 25, 30]])
+
+vector = np.array([1, 2, 3])
+result = matrix - vector
+print(result)
+
+matrix = np.array([[10, 20, 30],
+                   [40, 50, 60]])
+
+vector = np.array([10, 10, 10])
+result = matrix / vector
+print(result)
