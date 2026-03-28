257a0d4ffc8a9544b769fd34118dded39af2cba8
Farhanbuilds88 <farhanali510594@gmail.com>
2026-03-28 23:44:19 +0500

    added p2.py file

---
+++ vectorization/p2.py
@@ -0,0 +1,16 @@
+import numpy as np
+marks=np.array([40,80,70,86])
+result=marks*2
+print(result)
+
+prices=np.array([20,34,5,67])
+new_prices=prices*2
+print(prices)
+
+even_numbers=np.array([2,3,4,5,6])
+check=even_numbers%2==0
+print(check)
+
+greater_values=np.array([5,6,7,10,15])
+value_check=greater_values>6
+print(value_check)
