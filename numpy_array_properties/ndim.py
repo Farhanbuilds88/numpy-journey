8295776229f5f4f7955b861ce94a69b5949f65f4
Farhanbuilds88 <farhanali510594@gmail.com>
2026-03-29 12:51:28 +0500

    file modified

--- numpy_array_properties/ndim.py
+++ numpy_array_properties/ndim.py
@@ -12,3 +12,9 @@ arr_3d=np.array([[[1,3,4,5]
                   ,[2,4,5,1],
                   [2,7,8,4]]])
 print(arr_3d.ndim)
+#3d
+first_array=np.array([[[2,3,4,5,6],[2,3,4,6,7],[3,4,5,6,6]],
+                      [[2,3,4,5,8],[4,5,6,7,3],[4,5,6,7,3]]])
+print(first_array.ndim)
+print(first_array.size)
+print(first_array.shape)
