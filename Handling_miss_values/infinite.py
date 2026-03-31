42c13ad2a20bdf37ef57babc50498eddab2d56b1
Farhanbuilds88 <farhanali510594@gmail.com>
2026-03-31 22:16:13 +0500

    Update infinite.py with np.isinf examples

--- Handling_miss_values/infinite.py
+++ Handling_miss_values/infinite.py
@@ -1,3 +1,9 @@
 import numpy as np
 arr=np.array([1,2,np.inf,4,-np.inf,6])
 print(np.isinf(arr))
+
+arr = np.array([10, 20, np.inf, 40])
+print(np.isinf(arr))
+
+arr = np.array([-5, -10, -np.inf, 15])
+print(np.isinf(arr))
