76c1a838298858943f29cfe7bf07108fc917172f
Farhanbuilds88 <farhanali510594@gmail.com>
2026-03-31 22:32:09 +0500

    Update isnan.py with NumPy missing value handling examples

--- Handling_miss_values/isnan.py
+++ Handling_miss_values/isnan.py
@@ -4,3 +4,8 @@ print(np.isnan(arr))
 
 new_arr=np.array([23,4,5,7,8,np.nan,np.nan])
 print(np.isnan(new_arr))  
+
+new1=np.array([45,6,7,8,9,0,np.nan,np.nan,np.nan,34,5,6,7,8])
+print(np.isnan(new1))
+check_array=np.array([2,3,4,5,6,np.nan,56,np.nan,np.nan,45,6,7,8])
+print(np.isnan(check_array))
